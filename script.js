1db7a34a529f7f7f0e9d88d3addfeed715d501e3
Oluwasegun Babalola <babalolaoluwasegun635@gmail.com>
2024-02-01 21:51:48 +0100

    ui update

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const bgColor = ["rgb(152, 152, 236)", "#5bf5a8", "#7ff55b", "#a5f55b", "#e6f55b
 const cells = document.querySelectorAll(".cell");
 const buttons = document.querySelectorAll(".btn");
 const welcome = document.querySelector("#welcome");
-const header = document.querySelector("header");
+const scoreboard = document.querySelector(".score-board");
 let ch;
 
 buttons.forEach(button => (button.addEventListener("click", () => {
     ch = button.textContent;
-    header.style.display = "flex";
+    scoreboard.style.display = "flex";
     container.style.display = "grid";
     welcome.style.display = "none";
 })))

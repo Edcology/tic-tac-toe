e259c2ddd207d30349df4e87c3570474c3e9d29e
Awofala Gbolahan <edawofal@gmail.com>
2024-02-01 16:31:17 +0100

    tic-tac-toe UI

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.querySelector("#container");
+const bgColor = ["rgb(152, 152, 236)", "#5bf5a8", "#7ff55b", "#a5f55b", "#e6f55b", "#f55bed", "#f55b81", "#60f55b"];
+const cells = document.querySelectorAll(".cell");
+ 
+
+cells.forEach(cell => (cell.addEventListener("click", () => {
+    cell.textContent = "X"
+})))
+
+    // for (let i = 0; i < bgColor.length; i++) {
+    //     setTimeout(() => {
+    //         container.style.background = bgColor[i]; 
+    //     }, i * 3000)
+        
+    // }

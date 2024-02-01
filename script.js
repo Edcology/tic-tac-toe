4b36e93f2f1e521145d02c25c5ee9d628b55e77b
Awofala Gbolahan <edawofal@gmail.com>
2024-02-01 20:26:54 +0100

    Updated the event listener to take in input from the welcome page

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const cells = document.querySelectorAll(".cell");
 const buttons = document.querySelectorAll(".btn");
 const welcome = document.querySelector("#welcome");
 const header = document.querySelector("header");
+let ch;
 
 buttons.forEach(button => (button.addEventListener("click", () => {
-    let ch = button.value;
+    ch = button.textContent;
     header.style.display = "flex";
     container.style.display = "grid";
     welcome.style.display = "none";
-    return ch
 })))
  
 
